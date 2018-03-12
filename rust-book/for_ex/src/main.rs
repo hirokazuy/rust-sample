47f68755e5990b5cf3feec37a687a63071745c93
Hien Ayatuki <hien.ayatuki@gmail.com>
2018-03-12 23:46:43 +0900

    add for example. array access and countdwon.

--- rust-book/for_ex/src/main.rs
+++ rust-book/for_ex/src/main.rs
@@ -8,6 +8,22 @@ fn while_ex() {
     }
 }
 
+fn for_ex() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("The value is: {}", element);
+    }
+}
+
+fn countdown() {
+    for number in (1..4).rev() {
+        println!("number: {}", number);
+    }
+}
+
 fn main() {
     while_ex();
+    for_ex();
+    countdown();
 }
