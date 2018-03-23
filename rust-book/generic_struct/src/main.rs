9363601c8beebc2680e9d1c99a994df469d45a48
Hien Ayatuki <hien.ayatuki@gmail.com>
2018-03-23 23:51:02 +0900

    add generic struct

---
+++ rust-book/generic_struct/src/main.rs
@@ -0,0 +1,17 @@
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+struct Point2<T, U> {
+    x: T,
+    y: U,
+}
+
+fn main() {
+    let p = Point { x: 5, y: 10 };
+    let p2 = Point { x: "5", y: "10" };
+    //    let p3 = Point { x: 5, y: 10.0 };  // compile error
+    let p4 = Point2 { x: 5, y: 10.0 };
+}
