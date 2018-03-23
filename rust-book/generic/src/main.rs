32deb8c5474185e90fa7f5d49c2af5d5ca185826
Hien Ayatuki <hien.ayatuki@gmail.com>
2018-03-23 23:42:38 +0900

    add generic version of largest function, but now it cannot compile

--- rust-book/generic/src/main.rs
+++ rust-book/generic/src/main.rs
@@ -1,5 +1,5 @@
 
-fn largest_i32(list: &[i32]) -> i32 {
+fn largest<T>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list.iter() {
@@ -7,28 +7,16 @@ fn largest_i32(list: &[i32]) -> i32 {
             largest = item;
         }
     }
-    largest
-}
-
-fn largest_char(list: &[char]) -> char {
-    let mut largest = list[0];
-
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-
     largest
 }
 
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {}", result);
 }
