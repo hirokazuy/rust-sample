a189e233ac50ed27654e927c0e84c679b68d18e1
Hien Ayatuki <hien.ayatuki@gmail.com>
2018-03-27 23:11:46 +0900

    add summarizable trait library and, example main code.

---
+++ rust-book/trait-summary/src/main.rs
@@ -0,0 +1,13 @@
+extern crate trait_summary;
+use trait_summary::{Tweet, Summarizable};
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summary());
+}
