3ad81cbb2b09daaceb2c5663e9b156497c7cf295
Hien Ayatuki <hien.ayatuki@gmail.com>
2018-03-27 23:16:51 +0900

    add summarizable trait implimentation

--- rust-book/trait-summary/src/main.rs
+++ rust-book/trait-summary/src/main.rs
@@ -1,6 +1,19 @@
 extern crate trait_summary;
 use trait_summary::{Tweet, Summarizable};
 
+struct WeatherForecast {
+    high_temp: f64,
+    low_temp: f64,
+    chance_of_precipitation: f64,
+}
+
+impl Summarizable for WeatherForecast {
+    fn summary(&self) -> String {
+        format!("The high will be {}, and the low will be {}. The chance of precipitaion is {}%.",
+                self.high_temp, self.low_temp, self.chance_of_precipitation)
+    }
+}
+
 fn main() {
     let tweet = Tweet {
         username: String::from("horse_ebooks"),
@@ -10,4 +23,11 @@ fn main() {
     };
 
     println!("1 new tweet: {}", tweet.summary());
+
+    let wf = WeatherForecast {
+        high_temp: 20.0,
+        low_temp: 7.3,
+        chance_of_precipitation: 30.0,
+    };
+    println!("1 new weather forecast: {}", wf.summary());
 }
